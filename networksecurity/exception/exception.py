836821a6b5558c2c7d461870afb4875b22e94116
MrDanish00 <danishamir00@gmail.com>
2026-09-19 09:35:44 +0500

    Logging and Exception Implemented

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,14 @@
+import sys
+from networksecurity.logging.logger import logging
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message, error_details:sys):
+        self.error_message = error_message
+        _,_,exc_tb = error_details.exc_info()
+
+        self.lineno = exc_tb.tb_lineno
+        self.file_name = exc_tb.tb_frame.f_code.co_filename
+
+    def __str__(self):
+        return "\nError occured in: \n Filename: [{0}] \n line number: [{1}] \n error message: [{2}]".format(self.file_name,self.lineno,self.error_message)
+
+
